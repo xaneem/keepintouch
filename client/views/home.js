86b363785db31c1e5e30da8746f1374e72bbb3ee
Saneem <xaneem@gmail.com>
2015-07-22 16:21:12 +0530

    Working on UI

--- client/views/home.js
+++ client/views/home.js
@@ -1,7 +1,30 @@
-Template.home.helpers({
-  myAppVariable: function() {
-    return Session.get('myAppVariable');
+Template.navbar.helpers({
+  name: function() {
+    return "John Doe";
   }
+});
+
+Template.home.helpers({
+  name: function() {
+    return "John Doe";
+  },
+
+  contacts: function() {
+
+    var people = [];
+
+    var person = {
+        name: "John Doe",
+        phone : "+91-9811-245-353",
+        email : "johndoe@example.com"
+      };
+
+    for (var i = 0; i < 50; i++) {
+      people.push(person);
+    };
+    return people;
+  }
+
 });
 
 Template.home.events({
