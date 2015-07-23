383d35c80e1f631f4f4f171954caabc09aae2588
meharmp <meharmp@gmail.com>
2015-07-23 20:31:10 +0530

    Add new contact form validation

--- client/views/home.js
+++ client/views/home.js
@@ -15,7 +15,32 @@ Template.home.events({
     Session.set("contact-search", event.target.value);
   },
 
-  'submit .new-contact' : function(event) {
+  'keyup #contact-name' : function(event,template) {
+    
+   // console.log(event.target.value.length);
+    if(event.target.value !="")
+     template.find("#contact-name").classList.remove("input-error");
+    else
+     template.find("#contact-name").classList.add("input-error");
+
+  },
+  
+  'input #contact-name' :function(event,template){
+   
+    if(event.target.value.length==50)
+     template.find("#contact-name").classList.add("input-error");
+    else
+     template.find("#contact-name").classList.remove("input-error");
+
+  },
+
+  'blur #contact-name' : function(event,template){
+
+    template.find("#contact-name").classList.remove("input-error");
+  },
+
+
+  'submit .new-contact' : function(event,template) {
 
     event.preventDefault();
 
@@ -23,11 +48,21 @@ Template.home.events({
     var phone = event.target.contactPhone.value;
     var email = event.target.contactEmail.value;
 
+    if (name==="") 
+    {
+     // console.log(template.find("#contact-name"));
+
+      template.find("#contact-name").classList.add("input-error");
+      
+    }
+    else {
+
     Meteor.call("addContact",name,phone,email);
 
     event.target.contactName.value = "";
     event.target.contactPhone.value = "";
     event.target.contactEmail.value = "";
+  }
   }
 
 });
